134f9602e6851db7c99ea27432677c2b082ede3c
Jakub Mazur <me@jkmazur.pl>
2020-07-11 13:00:07 +0200

    Initial Commit

---
+++ AlbumTracker/AlbumTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumTrackerApp.swift
+//  AlbumTracker
+//
+//  Created by Jakub Mazur on 11/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct AlbumTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlbumTracker/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AlbumTracker
+//
+//  Created by Jakub Mazur on 11/07/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
