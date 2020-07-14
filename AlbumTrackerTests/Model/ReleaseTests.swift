9aa2cd2bf54d2ea48622a81cb44bb0c66deeb95b
Jakub Mazur <me@jkmazur.pl>
2020-07-14 14:56:15 +0200

    add standard tests for release

--- AlbumTrackerTests/Model/ReleaseTests.swift
+++ AlbumTrackerTests/Model/ReleaseTests.swift
@@ -7,3 +7,38 @@
 
 import XCTest
 @testable import AlbumTracker
+
+class ReleaseTests: XCTestCase {
+	func testParsingSuccess() {
+		let expectation = self.expectation(description: #function)
+		MainRelease.fetch(endpoint: .release(1), session: DummyURLSession.default) { (results) in
+			switch results {
+			case .success(let release):
+				XCTAssertEqual(release.id, 4455)
+				XCTAssertEqual(release.title, "The Freewheelin' Bob Dylan")
+				expectation.fulfill()
+			case .failure(let error):
+				XCTFail(error.localizedDescription)
+			}
+		}
+		self.waitForExpectations(timeout: 2) { (error) in
+			XCTAssertNil(error)
+		}
+	}
+	
+	func testFailedRequest() {
+		let expectation = self.expectation(description: #function)
+		MainRelease.fetch(endpoint: .release(1), session: DummyURLSession.failed) { (results) in
+			switch results {
+			case .failure(let error):
+				XCTAssertNotNil(error)
+				expectation.fulfill()
+			case .success(let release):
+				XCTFail("Expected to throw error but found: \(release)")
+			}
+		}
+		self.waitForExpectations(timeout: 2) { (error) in
+			XCTAssertNil(error)
+		}
+	}
+}
