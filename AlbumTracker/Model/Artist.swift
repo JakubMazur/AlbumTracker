c7b627cda6cfb99508f148d68543d53a79b9d0cd
Jakub Mazur <JakubMazur@users.noreply.github.com>
2020-07-14 22:25:22 +0200

    add artist storage (#7)

--- AlbumTracker/Model/Artist.swift
+++ AlbumTracker/Model/Artist.swift
@@ -6,8 +6,16 @@
 //
 
 import Foundation
+import RealmSwift
+
+struct FetchRequest<Model, RealmObject: RealmSwift.Object> {
+	let transformer: (Results<RealmObject>) -> Model
+	// skippting sorting and predicates for now
+}
 
 final class Artist: Identifiable, ObservableObject, Decodable, API {
+	
+	static var dbIdentifier: String = "artists"
 	
 	let id: UInt
 	let name: String
@@ -23,6 +31,15 @@ final class Artist: Identifiable, ObservableObject, Decodable, API {
 		self.profile = profile
 		self.images = images
 		self.masterReleases = nil
+	}
+	
+	fileprivate init(persistanceObject: ArtistPersistance) {
+		self.id = UInt(persistanceObject.artistID) ?? 0
+		self.name = persistanceObject.name
+		self.realName = persistanceObject.realName
+		self.profile = persistanceObject.profile
+		self.images = []
+		self.masterReleases = persistanceObject.masterReleases.compactMap { Release(persistanceObject: $0) }
 	}
 	
 	lazy var primaryImage: APIImage? = {
@@ -48,6 +65,44 @@ final class Artist: Identifiable, ObservableObject, Decodable, API {
 	}
 }
 
+extension Artist {
+	func save() throws {
+		let database = try Database(realm: nil)
+		try database.update(model: self, with: ArtistPersistance.init)
+	}
+	
+	#warning("this should be fetch(id: ...) and predicate on fetchrequest lvl")
+	static func fetch() throws -> [Artist]? {
+		let database = try Database(realm: nil)
+		let request = FetchRequest<[Artist], ArtistPersistance> { (results) -> [Artist] in
+			results.map { Artist(persistanceObject: $0) }
+		}
+		let fethed = database.fetch(with: request)
+		return fethed
+	}
+}
+
+final class ArtistPersistance: RealmSwift.Object {
+	@objc dynamic var artistID: String = UUID().uuidString
+	@objc dynamic var name: String = ""
+	@objc dynamic var realName: String = ""
+	@objc dynamic var profile: String = ""
+	let masterReleases: List<ReleasePersistance> = List<ReleasePersistance>()
+	
+	override static func primaryKey() -> String? {
+		return "artistID"
+	}
+	
+	convenience init(artist: Artist) {
+		self.init()
+		self.artistID = String(artist.id)
+		self.name = artist.name
+		self.realName = artist.realName
+		let releases = artist.masterReleases?.compactMap { ReleasePersistance(release: $0) } ?? []
+		self.masterReleases.append(objectsIn: releases)
+		self.profile = artist.profile
+	}
+}
 
 extension Artist {
 	static let testData: Artist = Artist(name: "Bob Dylan", id: 59792, realName: "Robert Allen Zimmerman", profile: "Born: May 24, 1941, Duluth, Minnesota, USA; singer, songwriter, \"song and dance man\".\r\nInducted into Songwriters Hall of Fame in 1982 and the Rock And Roll Hall of Fame in 1988 (Performer). Winner of the 2016 Nobel Prize in literature.\r\nHis first marriage was to Sara Dylan (November 1965 - divorced June 1977), together they have five children, including Jesse Dylan and Jakob Dylan. His second marriage was to Carolyn Dennis (4 June 1986 - divorced 7 August 1990).", images: [APIImage].testData)
